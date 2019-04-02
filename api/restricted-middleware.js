e7a840b4ad43a467c8f1dbf99de67ca69c1a1186
umbur <you@example.com>
2019-04-02 16:48:40 -0600

    refactored code, tested endpoints with Insomnia, the Postman was having issues

---
+++ api/restricted-middleware.js
@@ -0,0 +1,13 @@
+module.exports = (req, res, next) => {
+    try {
+      // if this throws, please don't crash my app
+      if (req && req.session && req.session.user) {
+        next();
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'you broke it!' });
+    }
+  };
+   
--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const KnexSessionStore = require('connect-session-knex')(session);
 const dbConfig = require('../dbConfig.js');
+const restricted = require('./restricted-middleware.js');
 
 
 const Users = require('../helpers/model.js')
@@ -14,22 +15,21 @@ server.use(cors());
 server.use(helmet());
 
 const sessionConfig = {
-    name: 'someName',
+    name: 'monster',
     secret: 'the secret message',
     cookie: {
-        maxAge: 1000 * 60 * 120,
-        secure: false // it's fine to use over http for dev
+        maxAge: 1000 * 60 * 10,
+        secure: false, // use cookie over https
+        httpOnly: true, 
     },
-    httpOnly: true, 
-    resave: false,
-    saveUninitialized: false,
-
+    resave: false, // avoid recreating unchanged sessions
+    saveUninitialized: false, // GDPR compliance
     store: new KnexSessionStore({
         knex: dbConfig,
         tablename: 'sessions',
         sidfieldname: 'sid',
         createtable: true,
-        clearInterval: 1000 * 60 * 120,
+        clearInterval: 1000 * 60 * 120, // delete expired sessions
     })
 }
 server.use(session(sessionConfig));
@@ -40,7 +40,7 @@ server.use(session(sessionConfig));
 server.post('/api/register', (req, res) => {
     let user = req.body;
     if (user.username && user.password) {
-        const hash = bcrypt.hashSync(user.password,4);
+        const hash = bcrypt.hashSync(user.password, 4);
         user.password = hash;
         Users.add(user)
         .then(saved => {
@@ -56,37 +56,31 @@ server.post('/api/register', (req, res) => {
 
 // POST login endpoint
 server.post('/api/login', (req, res) => {
-    let user = req.body;
-    if (user.username && user.password) {
-        Users.findBy({"username": user.username})
-        .first()
-        .then(info => {
-            if (info && bcrypt.compareSync(user.password, info.password)) {
-                req.session.user = user;
-                res.status(200).json({cookie: req.session.cookie, info, message: 'Logged in'})
-            } else {
-                res.status(401).json({message: "Invalid credentials"})
-            }
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err);
-        })
-    } else {
-        res.status(400).json({message: "Please give a username and password"})
-    }
-})
+    let {username, password } = req.body;
 
-// Middleware to restrict access
-function restricted(req, res, next) {
-    if (req.session && req.session.user) {
-        next();
-    } else {
-        res.status(401).json({message: "You shall not pass!"})
-    }
-}
+    Users.findBy({ username })
+    .first()
+    //console.log(username)
+    .then(user => {
+        //console.log(user)
+        if(user && bcrypt.compareSync(password, user.password)){
+           // req.session is added by express-session
+           req.session.user = user;
+           res.status(200).json({message: `Welcome ${user.username}!`}); 
+        } else {
+            res.status(401).json({message: 'Invalid Credentials!'})
+        }
+    }) .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal error!'
+           // error: error,
+        });
+    });
+});
 
-// GET users endpoint 
+
+// GET users endpoint with the restricted middleware 
 server.get('/api/users', restricted, (req, res) => {
     Users.find()
     .then(users => {
@@ -97,16 +91,16 @@ server.get('/api/users', restricted, (req, res) => {
 
 // GET endpoint to logout
 server.get('/api/logout', (req, res) => {
-    if (req.session) {
+    if(req.session) {
         req.session.destroy(err => {
-            if (err) {
-                console.log(err)
+            if(err) {
+                res.status(500).json({message: 'Error loging out!'})
             } else {
-                res.send('Goodbye!')
+                res.status(200).json({message: 'You have successfuly loged out!'})
             }
-        })
+        });
     } else {
-        res.end();
+        res.status(200).json({message: 'You have successfuly loged out!'})
     }
 })
    
